702b717b5d49bc51bbde6c836f740cf3b95f20f4
a6ka <a6ka666@gmail.com>
2016-10-17 00:39:48 +0300

    added CRUD-s

--- backend/views/site/index.php
+++ backend/views/site/index.php
@@ -2,6 +2,8 @@
 
 /* @var $this yii\web\View */
 
+use yii\helpers\Url;
+
 $this->title = 'My Yii Application';
 ?>
 <div class="site-index">
@@ -12,17 +14,17 @@ $this->title = 'My Yii Application';
             <div class="col-lg-4">
                 <h2>Projects</h2>
 
-                <p><a class="btn btn-default" href="http://www.yiiframework.com/doc/"><i class="fa fa-sitemap" aria-hidden="true"></i> All projects</a></p>
+                <p><a class="btn btn-default" href="<?= Url::to('admin/projects')?>"><i class="fa fa-sitemap" aria-hidden="true"></i> All projects</a></p>
             </div>
             <div class="col-lg-4">
                 <h2>Tasks</h2>
 
-                <p><a class="btn btn-default" href="http://www.yiiframework.com/doc/"><i class="fa fa-tasks" aria-hidden="true"></i> All tasks</a></p>
+                <p><a class="btn btn-default" href="<?= Url::to('admin/tasks')?>"><i class="fa fa-tasks" aria-hidden="true"></i> All tasks</a></p>
             </div>
             <div class="col-lg-4">
                 <h2>Statuses</h2>
 
-                <p><a class="btn btn-default" href="http://www.yiiframework.com/doc/"><i class="fa fa-flag" aria-hidden="true"></i> All task statuses</a></p>
+                <p><a class="btn btn-default" href="<?= Url::to('admin/statuses')?>"><i class="fa fa-flag" aria-hidden="true"></i> All task statuses</a></p>
             </div>
         </div>
 
